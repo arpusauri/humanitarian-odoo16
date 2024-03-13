95cb5b963fbaa559f16e59e266d4b7fb0122e3ce
arpusauri <aryapurnama877@gmail.com>
2024-03-13 14:19:03 +0700

    update widgets

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@ from odoo import models, fields
 class City(models.Model):
     _name = "humanitarian.city"
     _description = 'City'
+    _rec_name = 'city_id'
 
-    name = fields.Char(string="City")
+    city_id = fields.Char(string="City")
     sitrep_id = fields.Many2one('humanitarian.sitrep', string="Sitrep ID")
