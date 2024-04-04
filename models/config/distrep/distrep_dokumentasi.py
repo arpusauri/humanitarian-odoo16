fb3577636c517e3e412914bf0763ca02e55765b9
arpusauri <aryapurnama877@gmail.com>
2024-04-04 13:15:16 +0700

    fix image url

--- models/config/distrep/distrep_dokumentasi.py
+++ models/config/distrep/distrep_dokumentasi.py
@@ -5,12 +5,4 @@ class DistrepDocumentation(models.Model):
     _description = 'Dokumentasi Distribution Reports'
 
     image = fields.Binary(string="Image")
-    image_url = fields.Char('Image URL', compute='get_image_url')
     dok_dist_id = fields.Many2one('humanitarian.humanitarian_distrep', string='Distribution')
-
-
-@api.depends('image')
-def get_image_url(self):
-    web_base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-    for record in self:
-        record.image_url = f"{web_base_url}/web/image?model={self._name}&id={record.id}&field=image"
